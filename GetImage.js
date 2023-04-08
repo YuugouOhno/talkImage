3f5b5ebafba66acf3e32161d6552b766da413def
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-08 15:34:12 +0900

    モバイルに対応

--- GetImage.js
+++ GetImage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from "react";
 import { Image, Button, TextInput, View, Text } from 'react-native';
 import { OPEN_AI_API_KEY } from 'dotenv';
+import 'react-native-url-polyfill/auto';
 // import { MyImage } from './assets/MyImage.png'
 
 const { Configuration, OpenAIApi } = require("openai");
@@ -24,18 +25,21 @@ const GetImage = () => {
             n: 1,
             size: "256x256",
         }
-        const response = await openai.createImage(imageParameters);
-        const urlData = response.data.data[0].url
-        setImageUrl(urlData);
-        console(urlData);
+        try {
+            const response = await openai.createImage(imageParameters);
+            const urlData = response.data.data[0].url
+            setImageUrl(urlData);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
         <View>
             {
                 imageUrl
-                    ? <Image style={{ width: 100, height: 100 }}source={imageUrl} />
-                    : <Image style={{ width: 100, height: 100 }}source={MyImage} />
+                    ? <Image style={{ width: 100, height: 100 }} source={{ uri: imageUrl }} />
+                    : <Image style={{ width: 100, height: 100 }} source={MyImage} />
             }
             <TextInput placeholder='プロンプトの入力' value={userPrompt} onChangeText={(value) => setUserPrompt(value)} />
             <Button title="Generate" onPress={generateImage} />
