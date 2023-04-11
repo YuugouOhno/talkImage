ba41ac3075ba043f97b9262831198b01aaf8b83a
Shuma-Yamamoto <s.yamamoto.eng@gmail.com>
2023-04-11 10:17:34 +0900

    add: ブラウザでは上手くいったソースコード

--- src/modules/Ranking.js
+++ src/modules/Ranking.js
@@ -1,13 +1,59 @@
-// ここでAPI叩いて下さい、一旦App.jsに反映してある
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import axios from 'axios';
 
 const Ranking = () => {
-    console.log("こんにちは世界")
-    return (
-        <View>
-            <Text>はろーわーるど</Text>
+  const [file, setFile] = useState(null);
+  const [ranking, setRanking] = useState(null);
+
+  // 選択したファイルをセットする
+  const selectFile = async () => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type: 'text/plain',
+    });
+    if (result.type === 'success') {
+      setFile(result.file);
+    }
+  };
+
+  // リクエストの作成
+  const submitFile = async () => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // リクエストの送信＆レスポンスの受け取り
+    try {
+      const response = await axios.post('https://word-ranking-api.herokuapp.com/api', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      const responseJson = JSON.parse(response.data.content);
+      setRanking(responseJson)
+      console.log(responseJson);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View>
+      <Text>Word Ranking.</Text>
+      <Button title="ファイルを選択してください" onPress={selectFile} />
+      {file && (
+        <Text>選択されたファイル：{file.name}</Text>
+      )}
+
+      <Button title="送信" onPress={submitFile} disabled={!file} />
+      {ranking && (
+        ranking.map((item, index) => (
+        <View key={index}>
+          <Text>{item.rank}位「{item.word}」（{item.num_of_use}回）</Text>
         </View>
-    )
+      )))}
+    </View>
+  );
 }
 
-export default Ranking;
+export default Ranking
