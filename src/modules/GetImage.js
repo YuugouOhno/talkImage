3b7c2fe4863d6eac2552830027f8df7de9ca3c8a
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-13 09:47:39 +0900

    一応動作、日本語から画像を生成

--- src/modules/GetImage.js
+++ src/modules/GetImage.js
@@ -3,7 +3,11 @@ import { useState, useEffect } from "react";
 import { Image, Button, TextInput, View, Text } from 'react-native';
 import { OPEN_AI_API_KEY } from 'dotenv';
 import 'react-native-url-polyfill/auto';
-import Translate from './Translate';
+
+import axios from 'axios';
+import { TRANSLATE_KEY } from 'dotenv';
+
+
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
@@ -16,13 +20,19 @@ const GetImage = () => {
     const [userPrompt, setUserPrompt] = useState("")
     const [imageUrl, setImageUrl] = useState("")
     const [test, setTest] = useState("")
+    const [ranking, setRankings] = useState("")
+    
 
     useEffect(() => {
         async function translatePrompt() {
-            console.log("translatePrompt")
-            // const translateResult = Translate()
-            const translateResult = "apple"
-            setUserPrompt(translateResult)
+            const url = `https://api-free.deepl.com/v2/translate?auth_key=${TRANSLATE_KEY}&text=${ranking}&target_lang=EN`;
+            axios.post(url)
+                .then(response => {
+                    setUserPrompt(response.data.translations[0].text);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
         translatePrompt()
     }, [test]);
@@ -51,18 +61,6 @@ const GetImage = () => {
     const handleButtonClick = async () => {
         console.log("handleButtonClick")
         setTest("クリックされました")
-        // const imageParameters = {
-        //     prompt: userPrompt,
-        //     n: 1,
-        //     size: "256x256",
-        // }
-        // try {
-        //     const response = await openai.createImage(imageParameters);
-        //     const urlData = response.data.data[0].url
-        //     setImageUrl(urlData);
-        // } catch (error) {
-        //     console.error(error);
-        // }
     }
 
     return (
@@ -72,7 +70,7 @@ const GetImage = () => {
                     ? <Image style={{ width: 100, height: 100 }} source={{ uri: imageUrl }} />
                     : <Image style={{ width: 100, height: 100 }} source={MyImage} />
             }
-            {/* <TextInput placeholder='プロンプトの入力' value={userPrompt} onChangeText={(value) => setUserPrompt(value)} /> */}
+            <TextInput placeholder='プロンプトの入力' value={ranking} onChangeText={(value) => setRankings(value)} />
             <Button title="Generate" onPress={handleButtonClick} />
             <Text>{test}</Text>
         </View>
