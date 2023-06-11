21566dadd5f2bb92147d3009e4b29eb707f5bac5
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-11 15:51:24 +0900

    動いた！！！！！！！！！！！！！

--- src/modules/WordCloud.js
+++ src/modules/WordCloud.js
@@ -1,8 +1,30 @@
 import { Image, Button, View, Text, TextInput } from 'react-native';
+import KeywordsCloud from './KeywordsCloud';
 const WordCloud = () => {
+    const keywords = [{
+        keyword: 'word1',
+        frequency: 120,
+        color: 'purple'
+    }, {
+        keyword: 'word1',
+        frequency: 80,
+        color: 'green'
+    }, {
+        keyword: 'word1',
+        frequency: 123,
+        color: 'yellow'
+    }, {
+        keyword: 'word1',
+        frequency: 123,
+        color: 'blue'
+    }, {
+        keyword: 'word1',
+        frequency: 123,
+        color: 'red'
+    }]
     return (
         <View>
-            <Text>wordcloud</Text>
+            <KeywordsCloud keywords={keywords} scale={200} largestAtCenter={false} drawContainerCircle={true} containerCircleColor={'gray'}/>
         </View>
     )
 }
