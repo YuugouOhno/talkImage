da7dea6ecc12c8c20e4a4e302be9d799abc8f768
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-11 14:10:20 +0900

    wordcloud開発環境の整備

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import GetImage from './GetImage';
+import WordCloud from './WordCloud';
 
 const Home = () => {
     return (
         <View style={{ flex: 1, backgroundColor: 'white' }}>
-          <GetImage />
-          <StatusBar style="auto" />
+          {/* <GetImage />
+          <StatusBar style="auto" /> */}
+          <WordCloud/>
         </View>
 )};
 
---
+++ src/modules/WordCloud.js
@@ -0,0 +1,10 @@
+import { Image, Button, View, Text, TextInput } from 'react-native';
+const WordCloud = () => {
+    return (
+        <View>
+            <Text>wordcloud</Text>
+        </View>
+    )
+}
+
+export default WordCloud
