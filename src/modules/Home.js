e2b486256893514140767e76d5388fb884b3c7d4
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-14 11:43:25 +0900

    word cloudをlineのトークから出力

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import GetImage from './GetImage';
-import WordCloud from './WordCloud';
 
 const Home = () => {
     return (
         <View style={{ flex: 1, backgroundColor: 'white' }}>
           <GetImage />
           <StatusBar style="auto" />
-          {/* <WordCloud/> */}
         </View>
 )};
 
---
+++ src/modules/getRandomColorCode.js
@@ -0,0 +1,12 @@
+const getRandomColorCode = () => {
+    const letters = '0123456789ABCDEF';
+    let colorCode = '#';
+    
+    for (let i = 0; i < 6; i++) {
+      colorCode += letters[Math.floor(Math.random() * 16)];
+    }
+    
+    return colorCode;
+};
+
+export default getRandomColorCode
